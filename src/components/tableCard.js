786934e1ec6f3e221a046f791dcc45589986417f
ashNotKetchup <84208677+ashNotKetchup@users.noreply.github.com>
2023-11-17 00:55:25 +0000

    Simplified home page massively

--- src/components/tableCard.js
+++ src/components/tableCard.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-
-
-
 const TableCard = (props) => {
     // const length = Object.keys(props).length;
     const textSize = props.textSize;
     return (
-                <p class="">{props.first}</p> 
-
+                <>{props.first}</> 
     );
 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import TableCard from "../components/tableCard";
 import TagSelector from "../components/tagSelector";
 // import ParallelogramHeader from "../components/parallelogramHeader";
 import { StaticImage } from "gatsby-plugin-image"
+// import { groupBy} from 'lodash';
 
 
 // Return structured content for table card
@@ -38,22 +39,57 @@ const IndexPage = ({ pageContext }) => {
             title
             }
           }
-    work: allMarkdownRemark(
-    filter: {fields: {category: {eq: "work"}}}
-    sort: {frontmatter: {date: DESC}}
-    ) {
-      nodes {
-        fields {
-          slug
-        }
-        frontmatter {
-          title
-          tags
-          date(formatString: "ddd DD MMM yy")
-        }
-        id
+  
+          sound: allMarkdownRemark(
+  filter: {fields: {category: {eq: "sound"}}}
+  sort: {frontmatter: {date: DESC}}
+  ) {
+    nodes {
+      fields {
+        slug
       }
+      frontmatter {
+        title
+        tags
+        date(formatString: "ddd DD MMM yy")
+      }
+      id
     }
+  }
+
+  design: allMarkdownRemark(
+  filter: {fields: {category: {eq: "design"}}}
+  sort: {frontmatter: {date: DESC}}
+  ) {
+    nodes {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        tags
+        date(formatString: "ddd DD MMM yy")
+      }
+      id
+    }
+  }
+
+  research: allMarkdownRemark(
+  filter: {fields: {category: {eq: "research"}}}
+  sort: {frontmatter: {date: DESC}}
+  ) {
+    nodes {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        tags
+        date(formatString: "ddd DD MMM yy")
+      }
+      id
+    }
+  }
 
     allTags: allMarkdownRemark(
           limit: 2000
@@ -67,74 +103,58 @@ const IndexPage = ({ pageContext }) => {
   }
   `);
 
-  const [filteredNodes, setFilteredNodes] = useState(data.work.nodes);
 
-  const getFilteredNodes = useCallback((nodes) => {
-    setFilteredNodes(nodes);
-  }, [setFilteredNodes]);
-
+  const themes = [{"name": 'Sound',"data": data.sound.nodes}, , {"name": 'Design',"data": data.design.nodes}, {"name": 'Research',"data": data.research.nodes}]
 
   const bio = (
     <div className="container diff">
       
       <Link to="/about">
-        {/* <Link to="/about"> */}
-      {/* <ParallelogramHeader
-        text="Me"
-        className="mb-0"
-      /> */}
-      {/* </Link> */}
       <div
         dangerouslySetInnerHTML={{ __html: data.about.html }}
         className="is-size-1 is-bold pt-6 pr-6 has-text-left diff mb-0 mt-0"
       >
-        {/* <p>{data.about.html}</p> */}
+
       </div>
-      {/* <div className="has-text-left"> */}
-        {/* <br></br> */}
-
-
-        {/* Add some mouseover thing which says read more when hover  */}
-          {/* <p class="subtitle is-size-7 diff">Read More</p> */}
-        
-      {/* </div> */}
       </Link>
     </div>
-    // <div className="column is-two-thirds-desktop is-full-tablet">
-    //   <Video videoSrcURL={data.about.frontmatter.video} width={"90%"}/>
-    //   </div> 
+
   );
 
   const works = (
-    <div>
-      {/* <Link to="/work">
-      <ParallelogramHeader
-        text="Work"
-        className="mb-0"
-      />
-      </Link> */}
-      
-      {/* <div className="lowerPadding"> </div> */}
-      {filteredNodes.map((blogentry) => (
-        <div
-          className="card-image is-size-2"
-          key={blogentry.id}
-        >
-          <Link to={blogentry.fields.slug}>
-            <TableCard
-              first={blogentry.frontmatter.title}
-              second={blogentry.frontmatter.date}
-            />
-          </Link>
-         
-        </div>
-        
-      )
-      )}
-       <TagSelector tags={data.allTags} nodes={data.work.nodes} data={data} callback={getFilteredNodes} initialState="selected" />
-    </div>
 
-  )
+    //loop through theme, then loop through each entry in theme
+    themes.map((theme)=> 
+      (
+        <div className="">
+          <Link to="/work">
+            <h1 className="is-uppercase bigTitle">{theme.name}</h1>
+            </Link>
+        
+        {/* <div className="lowerPadding"> </div> */}
+          {theme.data.map((blogentry) => 
+            (
+            // <div
+            // className="card-image is-size-2"
+            // key={blogentry.id}
+            // >
+              <Link to={blogentry.fields.slug} key={blogentry.id} className="listedEntry">
+                {/* Later, would be cool to add some small images next to each title, square thumbnails same height as text */}
+                <TableCard
+                  first={blogentry.frontmatter.title}
+                  second={blogentry.frontmatter.date}
+                />
+                {/* {blogentry.frontmatter.title} */}
+                </Link>
+              // </div>  
+              )
+            )
+            }
+          </div>
+        )
+      )  
+    )
+
 
   const garden = (
     <div>
@@ -142,7 +162,6 @@ const IndexPage = ({ pageContext }) => {
         Garden
       {/* <ParallelogramHeader
         text="Garden"
-        
         className="mb-0 is-size-1 is-size-1"
       /> */}
       </Link>
@@ -153,31 +172,14 @@ const IndexPage = ({ pageContext }) => {
   return (
     <Layout name="Index" crumbs={crumbs}>
       <section>
-        <div className="hero is-fullheight-with-navbar">
-          <StaticImage
+        {/* <div className="hero is-fullheight-with-navbar"> */}
+          {/* <StaticImage
             className="background"
             src="../images/me-film.jpg"
-          />
-          {/* todo: Make it so that scrolls page through each of these on mobile.
-          On desktop make them fill their thirds */}
-          <div className="columns is-multiline is-centre mt-auto">
-            <div className="column is-4-desktop is-full-tablet mt-auto">
-              {bio}
-            </div>
-            <div className="column is-1-desktop is-full-tablet mt-auto"></div>
-            <div className="column is-5-desktop is-full-tablet mt-auto" >
-              <></>
-              {works}
-            </div>
-            <div className="column is-2-desktop is-full-tablet mt-auto has-text-right-desktop">
-              {garden}
-            </div>
-          </div>
-        </div>
-
+          /> */}
+            {works}
+        {/* </div> */}
       </section>
-
-
     </Layout>
   );
 };
