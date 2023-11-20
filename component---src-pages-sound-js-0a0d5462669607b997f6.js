e7bedb0c18b8403fbb66dcefb6c7eaa4bfc57f95
ashNotKetchup <84208677+ashNotKetchup@users.noreply.github.com>
2023-11-20 22:25:00 +0000

    Updates

---
+++ component---src-pages-sound-js-0a0d5462669607b997f6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkc4dm_website=self.webpackChunkc4dm_website||[]).push([[146],{7072:function(e,t,n){n.r(t),n.d(t,{Head:function(){return d}});var a=n(7294),u=n(1883),c=n(8678),s=n(508);t.default=e=>{let{pageContext:t}=e;const n={name:"sound",data:(0,u.useStaticQuery)("1058727377").sound.nodes};return a.createElement(c.Z,{name:"Index"},a.createElement("section",null,a.createElement(s.Z,{theme:n.name,data:n.data})))};const d=()=>a.createElement("title",null,"Sound")}}]);
+//# sourceMappingURL=component---src-pages-sound-js-0a0d5462669607b997f6.js.map
