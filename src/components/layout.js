ca6501d18e3d4dd2334d933f54eaf5e28840914c
ashNotKetchup <84208677+ashNotKetchup@users.noreply.github.com>
2023-12-09 11:25:47 +0000

    implemented an accent colour (pink)
    Pink is now here! for bolds, and for code, and some other stuff sometimes. Anyway, now the css can edit markdown styles using stuff within the content tag from bulma

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,42 +3,49 @@ import {Link} from "gatsby"
 import "../style/bulmacustom.scss"
 // import { Breadcrumb } from "gatsby-plugin-breadcrumb"
 
-// const navItems = [
-//     {name: "info", link: "/info"},
-// ]
-// const socialMediaItems = [
-//   {
-//     iconName: "fa-twitter",
-//     link: "https://twitter.com/c4dm",
-//   },
-// ];
+const navItems = [
+    {content:<code>Artist, Designer, and PhD Researcher in Artificial Intelligence and Music</code>, 
+      link: "/info"},
+]
+const socialMediaItems = [
+  {
+    iconName: "fa-twitter",
+    link: "https://twitter.com/c4dm",
+  },
+];
 
 const Layout = ({children, nameInNav, crumbs, name, hero}) => {
 
   
     const navBar = (
-      <nav className="column is-3 has-text-centered-desktop" role="navigation" aria-label="main navigation">
+      <nav className="column is-2-desktop has-text-left-desktop" role="navigation" aria-label="main navigation">
         <Link to="/" className="is-uppercase logo">
           Ashley Noel-Hirst
           </Link>
         
-        {/* <div class="navbar-end">
+        <div class="navbar-end">
             {navItems.map((item) => (
-              <Link className="is-uppercase" to={item.link}> 
-                {item.name}
+              <Link className="" to={item.link}> 
+                {item.content}
                 </Link>
 
             ))}
-          </div> */}
+          </div>
+      </nav>
+    );
+
+    const endBar = (
+      <nav className="column is-2-desktop has-text-left-desktop" role="navigation">
       </nav>
     );
 
     return (
-      <body className="columns is-desktop">
+      <body className="columns is-0 is-centered is-desktop mx-1 my-1">
           {navBar}
           <main className="pageContent column is-6">
             {children}
             </main>
+            {endBar}
           </body>
     );
 }
